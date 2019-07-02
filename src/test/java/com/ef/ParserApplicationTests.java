9eb3e27a68b570414ab06540fc6b7cd97f5e6f35
Bashir Ahmed Rakib <bashir@therapservices.net>
2019-07-02 20:46:51 +0600

    Added simple db integration test

--- src/test/java/com/ef/ParserApplicationTests.java
+++ src/test/java/com/ef/ParserApplicationTests.java
@@ -1,8 +1,32 @@
 package com.ef;
 
+import com.ef.config.DBConfig;
+import com.ef.config.RunParamConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {DBConfig.class, RunParamConfig.class})
 public class ParserApplicationTests {
 
-	public void contextLoads() {
-	}
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+	public void loadDb() {
+        List<Integer> testDb = jdbcTemplate.query("SELECT 1 FROM DUAL", (rs, rowNum) -> rs.getInt("1"));
+        assert testDb.get(0) == 1;
+    }
 
 }
